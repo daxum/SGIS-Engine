b5b2251fc1c36f53af0f5acbdad4269cf1900664
daxum <daxum144@gmail.com>
2018-07-16 15:44:02 -0500

    Fixed most compile errors, commented out files that still don't work with new vertex format. New interface for RendererMemoryManager. Engine still non-functional.

--- src/RendererMemoryManager.hpp
+++ src/RendererMemoryManager.hpp
@@ -18,11 +18,32 @@
 
 #pragma once
 
-#include <cstdint>
 #include <vector>
+#include <cstdint>
 
-#include "Model.hpp"
-#include "Vertex.hpp"
+#include "VertexBuffer.hpp"
+
+enum class BufferUsage {
+	//Reference counted buffer located in gpu memory, if possible. Meshes remain in
+	//the buffer even if nothing references them, for possible reuse later.
+	//They are only evicted if memory runs out.
+	DEDICATED_LAZY,
+	//Similar to above, except unused meshes are immediately invalidated and
+	//have to be reuploaded if used again.
+	DEDICATED_SINGLE,
+	//This is just a stream buffer.
+	STREAM
+};
+
+//All the information needed to create a vertex buffer.
+struct VertexBufferInfo {
+	//Format of the buffer (vertex layout).
+	std::vector<VertexElement> format;
+	//Buffer usage, determines where the buffer is stored and the eviction policy.
+	BufferUsage usage;
+	//Size of the buffer.
+	uint64_t size;
+};
 
 //An interface to the rendering engine's memory manager.
 class RendererMemoryManager {
@@ -33,25 +54,43 @@ public:
 	virtual ~RendererMemoryManager() {}
 
 	/**
-	 * Adds a static mesh (vertices + indices) to be uploaded to the gpu at a later time.
-	 * @param vertices The vertices for the mesh to be uploaded.
-	 * @param indices The vertex indices for the mesh to be uploaded.
-	 * @return a MeshData struct that can be used for drawing.
-	 * @throw runtime_error if the memory manager has already been initialized.
+	 * Adds a vertex buffer to the memory manager. Currently also creates an index buffer as well.
+	 * @param name The name of the buffer.
+	 * @param info Initialization info for the buffer.
 	 */
-	virtual std::shared_ptr<RenderMeshObject> addStaticMesh(const std::vector<Vertex>& vertices, const std::vector<uint32_t>& indices) = 0;
+	virtual void addBuffer(const std::string& name, const VertexBufferInfo& info) = 0;
 
 	/**
-	 * Dynamically uploads text model data to the gpu. Must call free later!
-	 * @param vertices The vertices for the text model.
-	 * @param indices The indices for the text model.
-	 * @return model data with which to render the text.
+	 * Adds a mesh to the provided buffer, and creates any resources needed to render it.
+	 * @param name The name to store the mesh under.
+	 * @param buffer The buffer to add the mesh to.
+	 * @param vertexData The data to add to the buffer.
+	 * @param dataSize The size of the data to add, in bytes.
+	 * @param indices Indices for the mesh to load into an index buffer.
+	 * @throw std::runtime_error If the name already exists.
 	 */
-	virtual std::shared_ptr<RenderMeshObject> addTextMesh(const std::vector<TextVertex>& vertices, const std::vector<uint32_t>& indices) = 0;
+	virtual void addMesh(const std::string& name, const std::string& buffer, const unsigned char* vertexData, size_t dataSize, const std::vector<uint32_t>& indices) = 0;
 
 	/**
-	 * Marks the memory previously occupied by the model data as unused.
-	 * @param data The model data to free.
+	 * Gets whether the mesh is already present in the given buffer.
+	 * @param mesh The name of the mesh to check.
+	 * @param buffer The buffer to check for the name.
+	 * @return Whether a mesh with the given name exists in the buffer.
 	 */
-	virtual void freeTextMesh(const std::shared_ptr<RenderMeshObject> data) = 0;
+	virtual bool hasMesh(const std::string& mesh, const std::string& buffer) = 0;
+
+	/**
+	 * Adds another user for the given mesh.
+	 * @param mesh The name of the mesh.
+	 * @param buffer The name of the buffer the mesh resides in.
+	 */
+	virtual void addUser(const std::string& mesh, const std::string& buffer) = 0;
+
+	/**
+	 * Removes a user of the given mesh. If the mesh has no users and is not part of
+	 * a lazy removal buffer, the memory it used is freed.
+	 * @param mesh The mesh to remove a user for.
+	 * @param buffer The buffer that contains the mesh.
+	 */
+	virtual void removeUser(const std::string& mesh, const std::string& buffer) = 0;
 };
